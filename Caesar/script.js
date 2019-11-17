7279ddae777991dbe6e2fcad0876619622898237
WizeHive <wizehive@Seans-MacBook-Pro.local>
2019-11-17 15:39:39 -0500

    Updated headshot

--- Caesar/script.js
+++ Caesar/script.js
@@ -1,26 +1,61 @@
 $(document).ready(() => {
-//highlight nav element on scroll
-let $text = $('#encrypt');
-let $button = $('button');
+let alphabet = 'abcdefghijklmnopqrstuvwxyz';
 
-// $(window).on('scroll', () => {
-//   let $cur_pos = $(this).scrollTop();
-//
-//   $sections.each(() => {
-//     let $top = $(this).offset().top,
-//         $ottom = $top + $(this).outerHeight();
-//
-//     if ($cur_pos >= $top && $cur_pos <= $bottom) {
-//       $nav.find('a').removeClass('activeMenuItem');
-//       $nav.find('a[href="#'+$(this).attr('id')+'"]').addClass('activeMenuItem');
-//     }
-//   })
-// })
+function encrypt(){
+  let $text = $('#encrypt');
+  let $result = $('#result');
+  let $shift = $('#encryptTab input');
+  let newText = "";
+  let $char;
+  for ($char in $text.val()) {
+    let index = alphabet.indexOf($text.val()[$char]);
+    if(index <0){
+      newText+= $text.val()[$char];
+    }
+    else {
+      index = (index+parseInt($shift.val(),10)>alphabet.length-1)? index+parseInt($shift.val(),10)-(alphabet.length) : index+parseInt($shift.val(),10);
+      newText+= alphabet[index];
+    }
+  }
+  $result.val(newText);
+}
 
-$button.on('click', () => {
-  let $line;
-  for ($line in $text.val()) {
-    console.log($text.val());
+function decrypt(){
+  let $text = $('#cipher');
+  let $result = $('#decrypt');
+  let $shift = $('#decryptTab input');
+  let newText = "";
+  let $char;
+  for ($char in $text.val()){
+    let index = alphabet.indexOf($text.val()[$char]);
+    if(index <0){
+      newText+= $text.val()[$char];
+    }
+    else {
+      index = (index-parseInt($shift.val(),10)<0)? index-parseInt($shift.val(),10)+(alphabet.length) : index-parseInt($shift.val(),10);
+      newText+= alphabet[index];
+    }
+  }
+  $result.val(newText);
+}
+
+$('#encryptTab button').on('click', () => {
+  encrypt();
+});
+
+$('#decryptTab button').on('click', () => {
+  decrypt();
+});
+
+$('.tab button').on('click', function() {
+  let tabId = $(this).attr('data-tab');
+  $('.tab button').removeClass('active');
+  $(this).addClass("active");
+  $('.tabcontent').removeClass('active');
+  if (tabId ==1 ){
+    $("#encryptTab").addClass('active');
+  } else {
+    $("#decryptTab").addClass('active');
   }
 });
 
